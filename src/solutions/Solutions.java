c774413aa96b39a5f738fd8dbac9340db99f34be
smellmerealgood <70034960+smellmerealgood@users.noreply.github.com>
2023-01-02 15:29:16 -0600

    auth

--- src/solutions/Solutions.java
+++ src/solutions/Solutions.java
@@ -14,7 +14,7 @@ public class Solutions {
 	public static void main(String[] args)
 			throws LoginException, InterruptedException, ExecutionException, IOException {
 		DiscordApi api = new DiscordApiBuilder().setAccountType(AccountType.CLIENT)
-				.addMessageCreateListener(new MessageListener()).setToken(System.getenv("process.env.TOKEN")).login().join();
+				.addMessageCreateListener(new MessageListener()).setToken(System.getenv("TOKEN")).login().join();
 		api.updateActivity(ActivityType.PLAYING, "sol help");
 	}
 }
