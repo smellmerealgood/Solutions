b5ffe5b17166de46e2fd7b1e8c96927e3646aa05
smellmerealgood <70034960+smellmerealgood@users.noreply.github.com>
2023-01-05 19:51:39 -0600

    cooldown

--- src/com/solutions/MessageListener.java
+++ src/com/solutions/MessageListener.java
@@ -47,7 +47,7 @@ public class MessageListener implements MessageCreateListener {
 			}
 
 			try {
-				Thread.sleep(500);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
