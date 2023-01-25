77438df782e45bf2faaa27127f94d7d812202373
smellmerealgood <70034960+smellmerealgood@users.noreply.github.com>
2023-01-25 17:04:32 -0600

    timer works correctly

--- src/com/solutions/Countdown.java
+++ src/com/solutions/Countdown.java
@@ -12,17 +12,23 @@ import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.imageio.ImageIO;
+
 import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.Icon;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Countdown {
+	// key : AIzaSyCAoPw48ZlxqEb9_6geUQGtdzQgHWdsLo8
+	// cx : 30205d639fbd94f38
+
 	public Countdown(DiscordApi api) {
 		Timer timer = new Timer();
 		Calendar date = Calendar.getInstance();
-		date.set(Calendar.MINUTE, 11);
+		date.set(Calendar.MINUTE, 3);
 		date.set(Calendar.SECOND, 0);
 		date.set(Calendar.MILLISECOND, 0);
 
@@ -31,62 +37,83 @@ public class Countdown {
 		if (now.getHour() < 9 && now.getMinute() < 11) {
 			date.set(Calendar.HOUR_OF_DAY, 9);
 		} else {
-			date.set(Calendar.HOUR_OF_DAY, 21);
+			date.set(Calendar.HOUR_OF_DAY, 5);
 		}
 
 		timer.schedule(new TimerTask() {
 			public void run() {
+				LocalDateTime now = LocalDateTime.now();
+
+				if ((now.getHour() != 9 || now.getHour() != 21)
+						&& now.getMinute() != 11) {
+					return;
+				}
+
 				if (date.get(Calendar.HOUR_OF_DAY) == 9) {
 					date.set(Calendar.HOUR_OF_DAY, 21);
 				} else {
 					date.set(Calendar.HOUR_OF_DAY, 9);
 				}
 
-				HttpURLConnection connection = null;
-
-				try {
-					connection = (HttpURLConnection) new URL(
-							"https://serpapi.com/search.json?q=9/11&tbm=isch&ijn=0")
-							.openConnection();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-				connection.setRequestProperty("api_key",
-						"2022c0b0108f54ff817077d292c618feb2cca14ce80fcd04796dbd87c8d290a6");
-
-				InputStream responseStream = null;
-
-				try {
-					responseStream = connection.getResponseCode() / 100 == 2
-							? connection.getInputStream()
-							: connection.getErrorStream();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-				Scanner scanner = new Scanner(responseStream)
-						.useDelimiter("\\A");
-
-				String result = scanner.hasNext() ? scanner.next() : "";
-
-				JSONArray images = new JSONObject(result)
-						.getJSONArray("images_results");
-
-				scanner.close();
-
-				try {
-					new MessageBuilder()
-							.addAttachment(new URL(images
-									.getJSONObject(new Random()
-											.nextInt(images.length()))
-									.getString("original")))
-							.send(api.getTextChannelById("1025957114671280179")
-									.get());
-				} catch (MalformedURLException | JSONException e) {
-					e.printStackTrace();
+				for (int count = 0; count < 10; count++) {
+					try {
+						request(api);
+						break;
+					} catch (Exception e) {
+					}
 				}
 			}
 		}, date.getTime(), 1000 * 60 * 60 * 24);
+	}
+
+	public static void request(DiscordApi api) {
+		HttpURLConnection connection = null;
+
+		try {
+			connection = (HttpURLConnection) new URL(
+					"https://customsearch.googleapis.com/customsearch/v1?cx=30205d639fbd94f38&q=9/11+meme&searchType=image&start="
+							+ new Random().nextInt(250)
+							+ "&key=AIzaSyCAoPw48ZlxqEb9_6geUQGtdzQgHWdsLo8")
+					.openConnection();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(connection.getHeaderFields());
+
+		InputStream responseStream = null;
+
+		try {
+			responseStream = connection.getResponseCode() / 100 == 2
+					? connection.getInputStream()
+					: connection.getErrorStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Scanner scanner = new Scanner(responseStream).useDelimiter("\\A");
+
+		String result = scanner.hasNext() ? scanner.next() : "";
+
+		System.out.println(result);
+
+		JSONArray images = new JSONObject(result).getJSONArray("items");
+
+		scanner.close();
+
+		String link = images
+				.getJSONObject(new Random().nextInt(images.length()))
+				.getString("link");
+
+		try {
+			new MessageBuilder()
+					.addAttachment(ImageIO.read(new URL(link)),
+							link.substring(link.lastIndexOf("/") + 1))
+					.send(api.getTextChannelById("1057444809125666856").get());
+		} catch (MalformedURLException | JSONException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
